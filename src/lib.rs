bd60dd5c72fa37bf2884747d5888f84469dcac29
muffie <morthwarm@gmail.com>
2022-12-02 00:13:31 +0100

    fix: read by line to vector
    - Remove the BufferReader which add performance and extra security
    but it's not needed in the AoC context.
    - Return a vector of int, one int for each each line

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-use std::{
-    fs::File,
-    io::{self, BufRead},
-    path::Path,
-};
+use std::{fs, path::Path};
 
-pub fn read_lines_from_file<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+/// Read a file with element seperated by a new line
+/// Element are i32.
+///
+/// # Examples
+///
+/// ```
+/// use presets::read_by_line_to_vec;
+///
+/// let result = read_by_line_to_vec("./one_int_per_line.txt");
+/// assert_eq!(result, vec![1, 2, 3]);
+/// ```
+pub fn read_by_line_to_vec<P>(filename: P) -> Vec<i32>
 where
     P: AsRef<Path>,
 {
-    let file = File::open(filename)?;
-    let bufreader = io::BufReader::new(file);
-    Ok(bufreader.lines())
+    let lines: String = fs::read_to_string(filename).unwrap();
+    let trimmed: &str = lines.trim();
+    trimmed
+        .split_terminator("\n")
+        .map(|line| line.parse().unwrap())
+        .collect()
 }
